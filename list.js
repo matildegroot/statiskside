97e770fa51a675a1ba93db1450b1445e2619bc01
Magr <magr0006@stud.kea.dk>
2025-02-06 11:28:27 +0100

    product list fix

--- list.js
+++ list.js
@@ -8,47 +8,22 @@ fetch ("https://kea-alt-del.dk/t7/api/products?limit=10")
 .then (showProductList);
 
 function showProductList(data){
+    console.log ("Her kommer mine data", data)
     const markup = data
     .map(
         (product) =>
-            `
-      <div class="product_list_container">
-        <div>
-          <img src="IMG/IMG_0902.webp" alt="billede1" class="product1" />
-          <h3>Red Ring</h3>
-          <p>Accessories∥Women</p>
-          <h4>Prev. DKK 1200,-</h4>
-          <h4>Now DKK 1000,-</h4>
-          <h5><a href="product.html">Read more</a></h5>
-        </div>
-        <div>
-          <h3 class="udsolgt">Sold out</h3>
-          <img src="IMG/IMG_0906.webp" alt="billede2" class="product2" />
-          <h3>Gold Necklace</h3>
-          <p>Accessories∥Women</p>
-          <h4>DKK 700,-</h4>
-          <h5><a href="product.html">Read more</a></h5>
-        </div>
-        <div>
-          <img src="IMG/IMG_0904.webp" alt="billede3" class="product3" />
-          <h3>String Necklace</h3>
-          <p>Accessories∥Women</p>
-          <h4>Prev. DKK 400,-</h4>
-          <h4>Now DKK 200,-</h4>
-          <h5><a href="product.html">Read more</a></h5>
-        </div>
-        <div>
-          <h3 class="udsolgt">Sold out</h3>
-          <img src="IMG/IMG_0909.webp" alt="billede4" class="product4" />
-          <h3>Gold Bangle</h3>
-          <p>Accessories∥Women</p>
-          <h4>DKK 1300,-</h4>
-          <h5><a href="product.html">Read more</a></h5>
-        </div>
-      </div>
-      `
-    )
-    .join("");
+            `<article class="product_list_container">
+                <div>
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="billede1" class="product1" />
+                <h3>${product.productdisplayname}</h3>
+                <p>${product.category}∥${product.gender}</p>
+                <h4>Prev. DKK 1200,-</h4>
+                <h4>Now DKK ${product.price},-</h4>
+                <h5><a href="product.html">Read more</a></h5>
+                </div>
+            </article>`
+    ).join("");
+    console.log ("Min markup er", markup)
 
     if (listContainer){
         listContainer.innerHTML = markup;
